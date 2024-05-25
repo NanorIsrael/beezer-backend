713fbfed2c1b2dc52747cb80046d53543a1cc4ce
nanorisrael <israel.nanor@compozelabs.com>
2024-05-25 20:06:54 +0000

    Add route for creating songs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateSongDTO } from 'src/dto/create-song-dto';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(readonly songsService: SongsService) {}
   @Get()
   findAll() {
     return 'Hello world!';
+  }
+
+  @Post()
+  addSong(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.addSong(createSongDTO);
   }
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+  readonly songs;
+
+  constructor() {
+    this.songs = [];
+  }
+
+  addSong(song) {
+    this.songs.push(song);
+    return this.songs;
+  }
+}
